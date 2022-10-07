fadea4e5fe4cca5cd3cb769a5005b831bd144180
pouet <jm@Tfes>
2022-10-07 17:28:57 +0200

    recup des classes

--- incs/ircserv.hpp
+++ incs/ircserv.hpp
@@ -6,24 +6,20 @@
 /*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 17:00:08 by dvallien          #+#    #+#             */
-/*   Updated: 2022/10/07 11:32:05 by dvallien         ###   ########.fr       */
+/*   Updated: 2022/10/07 16:06:05 by dvallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IRCSERV_HPP
 # define IRCSERV_HPP
-# include <iostream>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <arpa/inet.h>
-# include <sys/types.h>
-# include <unistd.h>
-# include <cerrno>
-# include <map>
-# include <string>
 
-int receiveMsg(const int socket, std::string &buffer);
-int sendMsg(const int socket, std::string &str);
-int sendMsg(const int socket, const char * str);
+# include <iostream>
+# include <string>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <netdb.h>
 
 #endif
