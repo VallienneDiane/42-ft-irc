b3e56efd81c7c21777eab13986fa695bbbfc4a35
Alexi Marchal <amarchal@z4r2p3.42lyon.fr>
2022-10-07 16:01:16 +0200

    main second version

--- incs/ircserv.hpp
+++ incs/ircserv.hpp
@@ -3,18 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ircserv.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amarchal <amarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 17:00:08 by dvallien          #+#    #+#             */
-/*   Updated: 2022/10/07 11:32:05 by dvallien         ###   ########.fr       */
+/*   Updated: 2022/10/07 15:55:28 by amarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IRCSERV_HPP
 # define IRCSERV_HPP
+
 # include <iostream>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <sys/types.h>
+# include <string>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <netdb.h>
+
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dvallien <dvallien@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amarchal <amarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 13:53:40 by dvallien          #+#    #+#             */
-/*   Updated: 2022/10/06 16:24:22 by dvallien         ###   ########.fr       */
+/*   Updated: 2022/10/07 15:57:32 by amarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,47 +27,148 @@
 	//bind : attach socket to port and address (socket, struct SOCKADDR_IN, size struct)
 */
 
-int main(int ac, char **av) // ./ircserv [port] [passwd]
+int serverSetup()
 {
-	(void)ac;
-	(void)av;
-	// ********* CREATE A SOCKET ********* //
-	int listening = socket(AF_INET, SOCK_STREAM, 0); 
-	if (listening == -1)
+	int socketServer = socket(AF_INET, SOCK_STREAM, 0);		// Socket d'ecoute
+	struct sockaddr_in addrServer;							// in : ipv4  in6 : ipv6
+	addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addrServer.sin_family = AF_INET;
+	addrServer.sin_port = htons(30000);						// host to network
+	
+	// Demande de l'attachement local de la socket
+	if (bind(socketServer, (const struct sockaddr *)&addrServer, sizeof(addrServer)) == -1)
 	{
-		std::cerr << "Can't create a socket" << std::endl;
-		return (1);
+		std::cerr << "Can't bind" << std::endl;
+		return (-1);
 	}
-	// ********* BIND ADDR IP & PORT TO A SOCKET ********* //
-	struct sockaddr_in hint; 
-	hint.sin_family = AF_INET; 
-	// int port = atoi(av[1]);
-	hint.sin_port = htons(6000); 
-	hint.sin_addr.s_addr = INADDR_ANY; // "127.0.0.1"
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
-	// ********* LISTEN THE PORT ********* //
-	listen(listening, SOMAXCONN);
-	std::cout << "Please enter message :" <<  std::endl;
-	// ********* WAIT FOR CONNECTION ********* //
-	while(1)
+	
+	// Indique que la socket attend des connexions, fixe la taille de la liste d'attente
+	if (listen(socketServer, SOMAXCONN) == -1)								
 	{
-		sockaddr_in	clientaddr;
-		socklen_t clientSize = sizeof(clientaddr);
-		int clientSocket = accept(listening,(sockaddr*)&clientaddr, &clientSize);
-		if (clientSocket != 0)
+		std::cerr << "Can't listen" << std::endl;
+		return (-1);
+	}
+	return (socketServer);
+}
+
+int acceptConnection(int socketServer)
+{
+	int socketClient;
+	struct sockaddr_in addrClient;
+	socklen_t csize = sizeof(addrClient);
+	
+	// Accepte les demandes de connexion. Crée une nvlle socket connectée et renvoie un fd pour cette socket // Socket de dialogue
+	socketClient = accept(socketServer, (struct sockaddr *)&addrClient, &csize);
+	if (socketClient == -1)
+	{
+		std::cerr << "Can't accept" << std::endl;
+		return (-1);
+	}
+
+	// Pas utile, affiche juste des infos sur le client connecté	
+	char host[NI_MAXHOST];
+	char service[NI_MAXSERV];
+	memset(host, 0, NI_MAXHOST);
+	memset(service, 0, NI_MAXSERV);
+	if (getnameinfo((sockaddr*)&addrClient, csize, host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		std::cout << host << " connected on port " << service << std::endl;
+	}
+	else
+	{
+		std::cout << "Error" << std::endl;
+	}
+	
+	return (socketClient);
+}
+
+void	handleConnection(int socketClient, fd_set *currentSockets, fd_set *writeSockets)
+{
+	char	buffer[4096];
+	memset(buffer, 0, sizeof(buffer));
+	
+	// wait for client ot send data
+	int bytesReceived = recv(socketClient, buffer, 4096, 0);
+	if (bytesReceived == -1)
+	{
+		std::cerr << "Error in recv(), Quitting" << std::endl;
+		close(socketClient);
+		FD_CLR(socketClient, currentSockets);		// remove socket to the set of sockets we are watching
+		return ;
+	}
+	if (bytesReceived == 0)
+	{
+		std::cout << "Client disconnected" << std::endl;
+		close(socketClient);
+		FD_CLR(socketClient, currentSockets);		// remove socket to the set of sockets we are watching
+		return ;	
+	}
+	if (buffer[strlen(buffer) - 1] == '\n')
+	{
+		// std::cout << "Msg received : " << buffer << std::endl;
+		//////// Echo msg to clients
+		for (int i = 0; i < FD_SETSIZE; i++)
 		{
-			int n;
-			char buffer[256];
-			
-			bzero(buffer,256);
-    		fgets(buffer,255,stdin);
-    		n = write(clientSocket, buffer, strlen(buffer));
-    		bzero(buffer,256);
-    		n = read(clientSocket, buffer, 255);
-    		printf("%s\n", buffer);
-			// send(clientSocket, "Hello", 5, 0); FORMAT A TROUVER
-			close(clientSocket);
+			if (FD_ISSET(i, writeSockets))
+				if (i != socketClient)
+					send(i, buffer, bytesReceived + 1, 0);
+		}
+		// send(socketClient, buffer, bytesReceived + 1, 0);
+	}
+	return ;
+}
+
+int main(int ac, char **av)  // ./ircserv [port] [passwd]
+{
+	int socketServer = serverSetup();
+	if (socketServer == -1)
+		return (1);
+
+	fd_set currentSockets;
+	fd_set readSockets;
+	fd_set writeSockets;
+
+	// initialize current set
+	FD_ZERO(&currentSockets);				// initialize
+	FD_SET(socketServer, &currentSockets);	// add serverSocket to the set of sockets we are watching
+	
+	while (1)
+	{
+		readSockets = currentSockets;		// because select is destructive, it keeps only the sockets ready for reading/writing but we want to keep tracks of all sockets we are watching
+		writeSockets = currentSockets;
+		
+		// int socketCount = select(FD_SETSIZE, &readSockets, nullptr, nullptr, nullptr);
+		// std::cout << "socketCount = " << socketCount << std::endl;
+		if (select(FD_SETSIZE, &readSockets, &writeSockets, nullptr, nullptr) == -1)
+		{
+			std::cerr << "select() error" << std::endl;
+			return (1);
+		}
+		
+		for (int i = 0; i < FD_SETSIZE; i++)
+		{
+			if (FD_ISSET(i, &readSockets))
+			{
+				if (i == socketServer)
+				{
+					// this is a new connection
+					std::cout << "New connection requested" << std::endl;
+					int socketClient = acceptConnection(socketServer);
+					if (socketClient == -1)
+						return (1);
+					FD_SET(socketClient, &currentSockets);			// add a new clientSocket to the set of sockets we are watching
+					FD_SET(socketClient, &writeSockets);
+					send(socketClient, "Welcome to the server !\n", sizeof("Welcome to the server !\n") + 1, 0);
+				}
+				else
+				{
+					// std::cout << "something to do with connection " << i << std::endl;
+					handleConnection(i, &currentSockets, &writeSockets);							// do what we want to do with this connection
+				}
+			}
 		}
 	}
+	
+	close(socketServer); //// A faire dans un signalHandler
 	return (0);
 }
