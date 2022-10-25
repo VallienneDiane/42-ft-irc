3b4888599a65875ff00ca130886d24ee0ea4e7d2
Jajalecapouet <nicolas.jaros@proton.me>
2022-10-25 11:25:43 +0200

    bot fumier can connect

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -114,6 +114,7 @@ void	handleConnection(int socketClient, fd_set *currentSockets, fd_set *writeSoc
 	if (bytesReceived == -1)
 	{
 		std::cerr << "Error in recv(), Quitting" << std::endl;
+		userMap.erase(socketClient);
 		close(socketClient);
 		FD_CLR(socketClient, currentSockets); // remove socket to the set of sockets we are watching
 		return ;
