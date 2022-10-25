fe95c490931c10e7c8a71f93feb4fc07592d22e3
Jajalecapouet <nicolas.jaros@proton.me>
2022-10-25 11:24:22 +0200

    bot fumier can connect

--- bots/srcs/fumier.cpp
+++ bots/srcs/fumier.cpp
@@ -17,56 +17,62 @@ int sendMsg(const int socket, std::string str)
 	return (send(socket, str.data(), str.size(), 0));
 }
 
+int	connection(char **av)
+{
+	struct addrinfo	hints;
+	struct addrinfo *res = NULL;
+	int				connectSocket;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	if (getaddrinfo(av[1], av[2], &hints, &res))
+	{
+		std::cerr << "Can't get addr\n";
+		return -1;
+	}
+	connectSocket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (connectSocket == -1)
+	{
+		std::cerr << "Can't do socket\n";
+		freeaddrinfo(res);
+		return -1;
+	}
+	if (connect(connectSocket, res->ai_addr, (int)res->ai_addrlen) == SO_ERROR) {
+		std::cerr << "Can't connect to server\n";
+		close(connectSocket);
+		freeaddrinfo(res);
+		connectSocket = -1;
+	}
+	return connectSocket;
+}
+
 int main(int ac, char **av) {
 	if (ac < 3)
 	{
-		std::cerr << "I need IP + port as arguments\n";
+		std::cerr << "I need IP + port as arguments (+ pass if needed)\n";
 		return 1;
 	}
-	in_addr_t	ip = inet_addr(av[1]);
-	if (ip == INADDR_NONE)
-	{
-		std::cerr << "your IP argument is invalid\n";
-		return 1;
-	}
-	uint16_t	port = htons(std::atoi(av[2]));
-	int socketFumier = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in addrFumier;
-	unsigned int	sizeAddrFumier = sizeof (addrFumier);
-	addrFumier.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addrFumier.sin_family = AF_INET;
-	addrFumier.sin_port = htons(0);
-	if (bind(socketFumier, (const struct sockaddr *) &addrFumier, sizeAddrFumier) == -1)
-		std::cerr << "can't bind";
-	else
-		getsockname(socketFumier, (struct sockaddr *) &addrFumier, &sizeAddrFumier);
-	char host[NI_MAXHOST];
-	char service[NI_MAXSERV];
-	memset(host, 0, NI_MAXHOST);
-	memset(service, 0, NI_MAXSERV);
-	if (getnameinfo((sockaddr*)&addrFumier, sizeAddrFumier, host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-	{
-		std::cout << host << " port Fumier in use : " << service << std::endl;
-	}
-	else
-		std::cout << "Error" << std::endl;
 
-	struct sockaddr_in	addrServ;
-	addrServ.sin_addr.s_addr = ip;
-	addrServ.sin_family = AF_INET;
-	addrServ.sin_port = port;
-	int servSocket = connect(socketFumier, (const struct sockaddr *) &addrServ, sizeof (addrServ));
-	if (servSocket == -1)
+	int	connectSocket = connection(av);
+	if (connectSocket == -1)
 	{
 		std::cerr << "connection failed\n";
 		return 1;
 	}
-	std::string	connectMsg = "NICK fumier\r\nUSER k k k k\r\n";;
-	sleep(1);
-	sendMsg(servSocket, connectMsg);
-	while (1)
-		sleep (20);
-	close (servSocket);
-	close (socketFumier);
+	std::cout << "connected\n";
+	std::string	nick = "NICK fumierBot\r\n";
+	std::string user = "USER fumierBot 0 * :\r\n";
+	if (ac > 3) {
+		std::string pass = "PASS ";
+		pass += av[3];
+		pass += "\r\n";
+		sendMsg(connectSocket, pass);
+	}
+	sendMsg(connectSocket, nick);
+	sendMsg(connectSocket, user);
+	std::cout << "data send\n";
+	sleep(20);
 	return 0;
 }
